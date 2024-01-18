c4bed200e891707aec0271ff93a36cf5506b037a
jatiro322 <156962691+jatiro322@users.noreply.github.com>
2024-01-18 22:08:20 +0300

    Add files via upload

---
+++ itrust/src/Calculator.java
@@ -0,0 +1,61 @@
+import  java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Converter converter = new Converter();
+        String[] actions = {"+", "-", "/", "*"};
+        String[] regexActions = {"\\+", "-", "/", "\\*"};
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String exp = scn.nextLine();
+        int actionIndex=-1;
+        for (int i = 0; i < actions.length; i++) {
+            if(exp.contains(actions[i])){
+                actionIndex = i;
+                break;
+            }
+        }
+        if(actionIndex==-1){
+            System.out.println("Некорректное выражение");
+            return;
+        }
+        String[] data = exp.split(regexActions[actionIndex]);
+            if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+                int a, b;
+                boolean isRoman = converter.isRoman(data[0]);
+                if (isRoman) {
+                    a = converter.romanToInt(data[0]);
+                    b = converter.romanToInt(data[1]);
+                } else {
+                    a = Integer.parseInt(data[0]);
+                    b = Integer.parseInt(data[1]);
+                }
+                if (a>10){
+                    System.out.println("некоректное число");
+                    return;}
+                if (b>10){
+                    System.out.println("некоректное число");
+                    return;}
+                if (a<1){
+                    System.out.println("некоректное число");
+                    return;}
+                if (b<1){
+                    System.out.println("некоректное число");
+                    return;}
+                int result = switch (actions[actionIndex]) {
+                    case "+" -> a + b;
+                    case "-" -> a - b;
+                    case "*" -> a * b;
+                    default -> a / b;
+                };
+                if (isRoman) {
+                    System.out.println(converter.intToRoman(result));
+                } else {
+                    System.out.println(result);
+                }
+            } else {
+                System.out.println("Числа должны быть в одном формате");
+            }
+        }
+    }
+
+
